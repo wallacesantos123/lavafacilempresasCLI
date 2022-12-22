5ee3e09183c767c025aabaaa7ed63bf26e82c185
Wallace Santos <dalas1998.ws@gmail.com>
2022-12-21 21:10:42 -0300

    Update Menu.js

--- src/paginas/menu/Menu.js
+++ src/paginas/menu/Menu.js
@@ -1,10 +1,30 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 
 const Menu = function ({Navigation}) {
     return (
-        <Text>Hello World</Text>
+        <KeyboardAvoidingView style={Style.container}>
+            <View style={Style.opcoes}>
+                <TouchableOpacity>
+                    <Text>Inicio</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
+
+const Style = new StyleSheet.create({
+    container : {
+        width : '100%',
+        height : '100%'
+    },
+
+    opcoes : {
+        width : '100%',
+        height : '10%',
+        borderWidth : 1,
+        borderRadius : 10
+    }
+})
 
 export default Menu;

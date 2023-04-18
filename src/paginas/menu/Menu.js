a531aeb0e2ff6e9c6d1735679d44a3757a77b0d5
Wallace Santos <dalas1998.ws@gmail.com>
2023-04-17 22:25:47 -0300

    Update 17/04/2023

--- src/paginas/menu/Menu.js
+++ src/paginas/menu/Menu.js
@@ -4,14 +4,14 @@ import { Text, View, StyleSheet, KeyboardAvoidingView, TouchableOpacity } from '
 const Menu = function ({Navigation}) {
     return (
         <KeyboardAvoidingView style={Style.container}>
-            <View style={Style.opcoes}>
-                <TouchableOpacity style={Style.inicio}>
-                    <Text style={Style.txtinicio}>Inicio</Text>
-                </TouchableOpacity>
+            <View style={Style.painel}>
+                <Text style={Style.titulo}>Lava-Facil Empresas</Text>
+                <View style={Style.financeiro}> 
                 
-                <TouchableOpacity>
-                    <Text>Lavagens</Text>
-                </TouchableOpacity>
+                </View>
+                <View style={Style.carros}>
+
+                </View>
             </View>
         </KeyboardAvoidingView>
     )
@@ -23,25 +23,33 @@ const Style = new StyleSheet.create({
         height : '100%'
     },
 
-    opcoes : {
-        width : '100%',
-        height : '10%',
-        borderWidth : 1,
-        borderRadius : 10
-    },
-
-    inicio : {
-        borderWidth : 1,
-        width : '20%',
-        height : '100%',
+    painel : {
         alignItems : 'center',
-        borderRadius : 10
+        backgroundColor : '#000',
+        height : '25%'
     },
 
-    txtinicio : {
-        paddingTop : '20%',
+    titulo : {
         fontSize : 20,
-        fontWeight : 'bold'
+        fontWeight : 'bold',
+        color : '#FFF'
+    },
+
+    financeiro : {
+        width : '40%',
+        height : '80%',
+        backgroundColor : '#FFF',
+        alignSelf : 'baseline',
+        marginLeft : '1%',
+    },
+
+    carros : {
+        width : '58%',
+        height : '80%',
+        backgroundColor : '#E6E6E6',
+        alignSelf : 'baseline',
+        marginTop : '-31.7%',
+        marginLeft : '42%'
     }
 })
 
